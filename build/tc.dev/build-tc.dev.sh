0aa27fcb05bbffea46b85d6816817dcd23e04c1a
Sebastien Levy <sebastien_levy@mentor.com>
2020-05-28 10:23:42 +0200

    build
    - removed ssh key specificities in image building

--- build/tc.dev/build-tc.dev.sh
+++ build/tc.dev/build-tc.dev.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-docker build \
-  --tag tc.dev \
-  --build-arg SSH_SECRET_KEY="$(cat ~/.ssh/id_rsa)" \
-  --build-arg SSH_PUBLIC_KEY="$(cat ~/.ssh/id_rsa.pub)" .
+docker build --tag tc.dev .
